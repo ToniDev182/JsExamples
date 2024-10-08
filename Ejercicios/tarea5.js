aa4c5153523b4edef0cb3edcf7851ff8a565313e
ToniDev182 <a.atienza.cluster@gmail.com>
2024-10-08 11:29:31 +0200

    faltaban las comillas

--- Ejercicios/tarea5.js
+++ Ejercicios/tarea5.js
@@ -8,11 +8,11 @@ num2 = parseInt (num2);
 
 function mayorque(num1, num2){
     if(num1 > num2){
-        alert("El numero uno es el mayor")
+        alert("El numero uno es el mayor");
     }else if(num1 < num2){
-        alert("El numero dos es mayor")
+        alert("El numero dos es mayor");
     }else{
-        alert("Los numeros son iguales")
+        alert("Los numeros son iguales");
     }
 }
 
